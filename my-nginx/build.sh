0b25b16d8fe1cd9b2987364db42f96964582832c
Dup4 <lyuzhi.pan@gmail.com>
2021-11-05 02:24:35 +0800

    chore: ci support

--- my-nginx/build.sh
+++ my-nginx/build.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-VERSION=1.21.3.1
-
 # shellcheck disable=SC2086
 docker build \
 	--build-arg ENABLED_MODULES="ngx_http_substitutions_filter_module" \
-	-t dup4/nginx:${VERSION} \
+	-t dup4/nginx:latest \
 	. \
 	-f ../docker-nginx/modules/Dockerfile
