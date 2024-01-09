c25678c8cdd0839b904b904e582e48bdd00f2cb1
Dup4 <lyuzhi.pan@gmail.com>
2024-01-09 23:16:26 +0800

    fix: COPY geoip2-data
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- custom-nginx/modify_dockerfile.sh
+++ custom-nginx/modify_dockerfile.sh
@@ -2,4 +2,4 @@
 
 CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-echo "COPY ${CUR_DIR}/geoip2-data /etc/geoip2-data" >>"${CUR_DIR}"/../docker-nginx/modules/Dockerfile
+echo "COPY ${CUR_DIR}/../geoip2-data /etc/geoip2-data" >>"${CUR_DIR}"/../docker-nginx/modules/Dockerfile
