d4ad0bde265432d6e6e2139e981230cc4dec5a79
Dup4 <lyuzhi.pan@gmail.com>
2024-01-11 22:10:02 +0800

    style: format
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- custom-nginx/env.sh
+++ custom-nginx/env.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-# export ENABLED_MODULES="geoip2 ngx_http_substitutions_filter_module nginx-module-vts"
-export ENABLED_MODULES="brotli geoip2 headers-more lua ndk subs-filter nginx-module-vts"
+export ENABLED_MODULES="nginx-module-vts brotli geoip2 headers-more lua ndk subs-filter"
