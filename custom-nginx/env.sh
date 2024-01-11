022af24c1693b2dbf318ed8302729c02e1f22c9a
Dup4 <lyuzhi.pan@gmail.com>
2024-01-11 08:28:01 +0800

    feat: add more inline modules
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- custom-nginx/env.sh
+++ custom-nginx/env.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-export ENABLED_MODULES="geoip2 ngx_http_substitutions_filter_module nginx-module-vts"
+# export ENABLED_MODULES="geoip2 ngx_http_substitutions_filter_module nginx-module-vts"
+export ENABLED_MODULES="brotli geoip2 headers-more lua ndk subs-filter nginx-module-vts"
